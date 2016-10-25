25984d9ea5f6e805d3cdf0b5c10ca25a14548b65
Laure <laure.milian@gmail.com>
2016-10-25 10:18:24 +0200

    menu récupéré et ajouté

--- js/app.js
+++ js/app.js
@@ -5,41 +5,51 @@
 
 		init : function(){
 			this.getArticle();
-			this.getJson();
+			this.getMenu();
 		},
 		
+		// Pour récupérer et convertir article Alice :
+
 		getArticle : function() {
 			$.ajax('http://192.168.1.21:1337/alice.md')
 			.done(this.getArticleDone)
-			.fail(this.getArticleFail)
-			.always(this.getArticleAlways)
+			.fail(this.requestFail)
+			.always(this.requestAlways)
 		},
 
-		getArticleDone : function(response) {
-			console.log(response)
-
+		getArticleDone : function(articleAlice) {
 			var converter = new showdown.Converter();
-			var articleHtml = converter.makeHtml(response);
+			var articleHtml = converter.makeHtml(articleAlice);
 			$("#md").html(articleHtml);
 		},
 
-		getArticleFail : function() {
+		// Pour récupérer menu :
+
+		getMenu : function() {
+			$.ajax('http://192.168.1.21:1337/menu.json')
+			.done(this.getMenuDone)
+			.fail(this.requestFail)
+			.always(this.requestAlways)
+		},
+
+		getMenuDone : function(response) {
+			console.log(response.menu[0])
+			var len = response.menu.length;
+
+			for (var i = 0; i < len; i++) {
+				$("#menu").append('<h4> <a href="' + response.menu[i].path + '">' + response.menu[i].title + '</a></h4>');
+			}
+		},
+
+		// Fonctions communes requêtes :
+
+		requestFail : function() {
 			alert("fail");
 		},
 
-		getArticleAlways : function() {
+		requestAlways : function() {
 			console.log("complete");
-		},
-
-		getJson : function() {
-			$.ajax('http://192.168.1.21:1337/example.json')
-			.done(this.getJsonDone)
-		},
-
-		getJsonDone : function(response) {
-			console.log("JSON ?" + response)
 		}
-
 
 	};
 
